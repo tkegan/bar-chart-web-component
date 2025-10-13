de5468fd08af19d488c6aa59416afef6d7d874a2
Tom Egan <tom@tomegan.tech>
2025-10-13 07:46:09 -0400

    feature: add web component for encapsulating data points

---
+++ DataPoint.js
@@ -0,0 +1,33 @@
+/**
+ * DataPoint.js a webcomponent for encapsulating a dependent/indepenent value
+ * pair to be displayed as part of a bar chart.
+ *
+ * @author Tom Egan
+ * @license 2-Clause BSD NON-AI License
+ */
+
+class DataPoint extends HTMLElement {
+	static observedAttributes = ["label", "value"];
+
+	constructor() {
+		super();
+	}
+
+	get label() {
+		return this.getAttribute("label") ?? "";
+	}
+
+	set label(label) {
+		this.setAttribute("label", label);
+	}
+
+	get value() {
+		return this.getAttribute("value") ?? "";
+	}
+
+	set value(value) {
+		this.setAttribute("value", value);
+	}
+}
+
+customElements.define("data-point", DataPoint);
